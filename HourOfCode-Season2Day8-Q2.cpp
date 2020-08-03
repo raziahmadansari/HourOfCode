fd717ea3a9ed649df75469e10b59f4d4776cdab6
Razi Ahmad Ansari <48851944+raziahmadansari@users.noreply.github.com>
2020-08-03 14:55:46 +0530

    Update HourOfCode-Season2Day8-Q2.cpp

--- HourOfCode-Season2Day8-Q2.cpp
+++ HourOfCode-Season2Day8-Q2.cpp
@@ -18,12 +18,12 @@ int optimalStrategyOfGame(int* arr, int n)
         }
     }
 
-    for (int i = 0; i < n; i++)
+    /*for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
             cout << table[i][j] << " ";
         cout << endl;
-    }
+    }*/
 
     return table[0][n - 1];
 }
